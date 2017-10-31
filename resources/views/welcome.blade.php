c41c66847818d2d32864ae74b8a7d9676907c30d
iramacazo <ira_macazo@dlsu.edu.ph>
2017-10-30 22:30:02 -0700

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel BADING
                 </div>
 
                 <div class="links">
