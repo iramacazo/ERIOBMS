9ce4d5dbcf911afa8013315c4cf4060d5286d615
iramacazo <ira_macazo@dlsu.edu.ph>
2017-10-30 22:08:39 -0700

    Initial Commit

--- config/app.php
+++ config/app.php
@@ -177,6 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Laravel IDE Helper
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
