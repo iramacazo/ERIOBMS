d2ab625e806d481f06929b20bd2c175950844b5d
iramacazo <ira_macazo@dlsu.edu.ph>
2017-11-01 18:52:23 +0800

    Styled the login screen
    Still waiting for judgement

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,20 +1,50 @@
 <html>
 	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
 		<title> ERIO BMS </title>
-		<!-- Insert CSS -->
+
+		<!-- Bootstrap CSS and JS -->
+		<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <script src="{{asset('js/jquery-3.2.1.min.js')}}"></script>
+		<script src="{{asset('js/bootstrap.min.js')}}"></script>
+
+        <!-- Global CSS for Background Colours and fonts and stuff -->
+        <link rel="stylesheet" href="{{asset('css/global.css')}}">
+
+        <!-- Specific CSS -->
+        <link rel="stylesheet" href="{{asset('css/login.css')}}">
+
+        <!-- Follow the format of CSS and JS files so that the specifics can override the generals -->
 	</head>
 	<body>
-		Login
-		<form action="" method="POST"> <!-- Insert Action -->
-			<input type="text" name="username" placeholder="Username">
-			<input type="password" name="password" placeholder="Password">
-			<input type="submit" name="submit" value="submit" placeholder="Login">
-			{{ csrf_field() }}
-		</form>
-		<br>
-		<br>
-		<a href="{{ url('register') }}">Register</a> <!-- Remove Later? -->
-
-		<!-- Insert JS -->
+        <nav class="navbar navbar-fixed-top">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">
+                    <img class="nav-icon" src="{{asset('images/Logo.png')}}">
+                </a>
+            </div>
+        </nav>
+        <div class="container" id="main-body">
+            <div class="row">
+                <span class="col-md-2"></span>
+                <div id="login-block" class="col-md-8 align-middle">
+                    <form action="" method="POST" class="form-group"> <!-- Insert Action -->
+                        <label for="username">Username</label>
+                        <input class="form-control" type="text" name="username" placeholder="Username">
+                        <label for="password">Password</label>
+                        <input class="form-control" type="password" name="password" placeholder="Password">
+                        <input class="form-control" type="submit" name="submit" value="Login" id="login-button"
+                               placeholder="Login">
+                        {{ csrf_field() }}
+                    </form>
+                    <br>
+                    <p id="register-link">New Employee? <a href="{{ url('register') }}">Register here!</a></p>
+                </div>
+                <span class="col-md-2"></span>
+            </div>
+        </div>
 	</body>
 </html>
