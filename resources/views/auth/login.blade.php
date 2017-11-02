a615ef6ff8991e161ed29d7ae628338690e66c94
MylesAlveniz <myles.alveniz@yahoo.com>
2017-11-02 16:30:17 +0800

    Altered UI in Log In and Register page
    - Made the ribbon smaller
    - Moved the logo further left
    - Made the boxes smaller
    - Made the font size of the labels a bit smaller
    - Changed the margin of the labels
    - Removed the placeholders of input
    - Changed some of the margin and width of the buttons
    - Added required to the input
    - Added minlength to password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,44 +20,55 @@
     <!-- Follow the format of CSS and JS files so that the specifics can override the generals -->
 </head>
 <body>
-<nav class="navbar navbar-fixed-top">
-    <div class="navbar-header">
-        <a class="navbar-brand" href="/">
-            <img class="nav-icon" src="{{asset('images/Logo.png')}}">
-        </a>
-    </div>
-</nav>
-<div class="container" id="main-body">
-    <div class="row">
-        <span class="col-md-3"></span>
-        <div id="login-block" class="col-md-6 align-middle">
-            <form action="{{route('login')}}" method="POST" class="form"> <!-- Insert Action -->
-                <div class="form-group{{ $errors->has('username') ? ' has-danger' : '' }}">
-                    <label for="username">Username</label>
-                    <input class="form-control" type="text" name="username" value="{{old('username')}}"
-                           placeholder="Username">
-                    @if ($errors->has('username'))
-                        <div class="form-control-feedback">{{ $errors->first('username') }}</div>
-                    @endif
-                </div>
 
-                <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
-                    <label for="password" class="control-label">Password</label>
-                    <input class="form-control" type="password" name="password" placeholder="Password">
-                    @if ($errors->has('password'))
-                        <div class="form-control-feedback">{{ $errors->first('password') }}</div>
-                    @endif
-                </div>
-
-                <input class="form-control" type="submit" name="submit" value="Login" id="login-button"
-                       placeholder="Login">
-                {{ csrf_field() }}
-            </form>
-            <br>
-            <p id="register-link">New Employee? <a href="{{ url('register') }}">Register here!</a></p>
+    <!-- RIBBON BAR -->
+    <nav class="navbar navbar-fixed-top">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">
+                <img class="nav-icon" src="{{asset('images/Logo.png')}}">
+            </a>
         </div>
-        <span class="col-md-3"></span>
+    </nav>
+
+    <!-- BODY -->
+    <div class="container" id="main-body">
+        <div class="row">
+            <span class="col-md-3"></span>
+
+            <!-- LOG IN BOX -->
+            <div id="login-block" class="col-md-4">
+                <form action="{{route('login')}}" method="POST" class="form"> <!-- Insert Action -->
+
+                    <!-- USERNAME -->
+                    <div class="form-group{{ $errors->has('username') ? ' has-danger' : '' }}">
+                        <label for="username">Username</label>
+                        <input class="form-control" type="text" name="username" value="{{old('username')}}" required>
+                        @if ($errors->has('username'))
+                            <div class="form-control-feedback">{{ $errors->first('username') }}</div>
+                        @endif
+                    </div>
+
+                    <!-- PASSWORD -->
+                    <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                        <label for="password" class="control-label">Password</label>
+                        <input class="form-control" type="password" name="password" required>
+                        @if ($errors->has('password'))
+                            <div class="form-control-feedback">{{ $errors->first('password') }}</div>
+                        @endif
+                    </div>
+
+                    <!-- LOG IN BUTTON -->
+                    <input class="form-control" type="submit" name="submit" value="Login" id="login-button" placeholder="Login">
+                    {{ csrf_field() }}
+                </form>
+
+                <!-- REGISTER -->
+                <p id="register-link">New Employee? <br><a href="{{ url('register') }}">Register here!</a></p>
+            </div>
+
+            <span class="col-md-3"></span>
+        </div>
     </div>
-</div>
+
 </body>
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,87 +20,98 @@
     <!-- Follow the format of CSS and JS files so that the specifics can override the generals -->
 </head>
 <body>
-<nav class="navbar navbar-fixed-top">
-    <div class="navbar-header">
-        <a class="navbar-brand" href="{{ route('login') }}">
-            <img class="nav-icon" src="{{asset('images/Logo.png')}}">
-        </a>
-    </div>
-</nav>
-<div class="container" id="main-body">
-    <div class="row">
-        <span class="col-md-3"></span>
-        <div id="form-block" class="col-md-6">
-            <form action="{{ route('register') }}" method="POST" class="form">
-                <div class="form-group{{ $errors->has('firstname') ? ' has-danger' : '' }}">
-                    <label for="firstname" class="control-label">First Name</label>
-                    <input class="form-control" type="text" name="firstname" placeholder="First Name" required autofocus
-                           value="{{old('firstname')}}">
 
-                    @if ($errors->has('firstname'))
-                        <div class="form-control-feedback">{{ $errors->first('firstname') }}</div>
-                    @endif
-                </div>
-
-                <div class="form-group{{$errors->has('lastname') ? ' has-danger' : ''}}">
-                    <label for="lastname" class="control-label">Last Name</label>
-                    <input class="form-control" type="text" name="lastname" placeholder="Last Name" required
-                        value="{{old('lastname')}}">
-
-                    @if ($errors->has('lastname'))
-                        <div class="form-control-feedback">{{ $errors->first('lastname') }}</div>
-                    @endif
-                </div>
-
-                <div class="form-group{{$errors->has('username') ? ' has-danger' : ''}}">
-                    <label for="username" class="control-label">Username</label>
-                    <input class="form-control" type="text" name="username" placeholder="Username" required
-                        value="{{old('username')}}">
-
-                    @if($errors->has('username'))
-                        <div class="form-control-feedback">{{$errors->first('username')}}</div>
-                    @endif
-                </div>
-
-                <div class="form-group{{$errors->has('email') ? ' has-danger' : ''}}">
-                    <label for="email" class="control-label">E-mail Address</label>
-                    <input class="form-control" type="email" name="email" placeholder="E-mail Address" required
-                        value="{{old('email')}}">
-
-                    @if($errors->has('email'))
-                        <div class="form-control-feedback">{{$errors->first('email')}}</div>
-                    @endif
-                </div>
-
-                <div class="form-group{{$errors->has('password') ? 'has-error': ''}}">
-                    <label for="password" class="control-label">Password</label>
-                    <input class="form-control" type="password" name="password" placeholder="Password" required>
-
-                    @if($errors->has('password'))
-                        <div class="form-control-feedback">{{$errors->first('password')}}</div>
-                    @endif
-                    <small class="form-text text-muted">Minimum 6 characters</small>
-                </div>
-
-                <div class="form-group">
-                    <label for="password_confirmation" class="control-label">Retype Password</label>
-                    <input class="form-control" type="password" name="password_confirmation"
-                           placeholder="Retype Password" required>
-                </div>
-
-                {{-- <label for="usertype">User Type</label>
-                <input class="form-control" type="text" name="usertype" placeholder="User Type">--}}
-
-                <!-- Change to Radio Button -->
-                <input class="form-control" type="submit" name="submit" value="Register" id="register-button"
-                       placeholder="Register">
-                <small class="form-text text-muted">*All fields are required</small>
-                {{ csrf_field() }}
-            </form>
+    <!-- RIBBON BAR -->
+    <nav class="navbar navbar-fixed-top">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="{{ route('login') }}">
+                <img class="nav-icon" src="{{asset('images/Logo.png')}}">
+            </a>
         </div>
-        <span class="col-md-3"></span>
+    </nav>
+
+    <!-- BODY -->
+    <div class="container" id="main-body">
+        <div class="row">
+            <span class="col-md-3"></span>
+
+            <!-- REGISTER BOX -->
+            <div id="form-block" class="col-md-5">
+                <form action="{{ route('register') }}" method="POST" class="form">
+
+                    <!-- FIRST NAME -->
+                    <div class="form-group{{ $errors->has('firstname') ? ' has-danger' : '' }}">
+                        <label for="firstname" class="control-label">First Name</label>
+                        <input class="form-control" type="text" name="firstname" required autofocus
+                               value="{{old('firstname')}}">
+
+                        @if ($errors->has('firstname'))
+                            <div class="form-control-feedback">{{ $errors->first('firstname') }}</div>
+                        @endif
+                    </div>
+
+                    <!-- LAST NAME -->
+                    <div class="form-group{{$errors->has('lastname') ? ' has-danger' : ''}}">
+                        <label for="lastname" class="control-label">Last Name</label>
+                        <input class="form-control" type="text" name="lastname" required
+                            value="{{old('lastname')}}">
+
+                        @if ($errors->has('lastname'))
+                            <div class="form-control-feedback">{{ $errors->first('lastname') }}</div>
+                        @endif
+                    </div>
+
+                    <!-- USERNAME -->
+                    <div class="form-group{{$errors->has('username') ? ' has-danger' : ''}}">
+                        <label for="username" class="control-label">Username</label>
+                        <input class="form-control" type="text" name="username" required
+                            value="{{old('username')}}">
+
+                        @if($errors->has('username'))
+                            <div class="form-control-feedback">{{$errors->first('username')}}</div>
+                        @endif
+                    </div>
+
+                    <!-- EMAIL -->
+                    <div class="form-group{{$errors->has('email') ? ' has-danger' : ''}}">
+                        <label for="email" class="control-label">E-mail Address</label>
+                        <input class="form-control" type="email" name="email" required
+                            value="{{old('email')}}">
+
+                        @if($errors->has('email'))
+                            <div class="form-control-feedback">{{$errors->first('email')}}</div>
+                        @endif
+                    </div>
+
+                    <!-- PASSWORD -->
+                    <div class="form-group{{$errors->has('password') ? 'has-error': ''}}">
+                        <label for="password" class="control-label">Password</label>
+                        <input class="form-control" type="password" name="password" minlength = 6 required>
+
+                        @if($errors->has('password'))
+                            <div class="form-control-feedback">{{$errors->first('password')}}</div>
+                        @endif
+                        <small class="form-text text-muted">Minimum 6 characters</small>
+                    </div>
+
+                    <!-- RE PASSWORD -->
+                    <div class="form-group">
+                        <label for="password_confirmation" class="control-label">Retype Password</label>
+                        <input class="form-control" type="password" name="password_confirmation" required>
+                    </div>
+
+                    {{-- <label for="usertype">User Type</label>
+                    <input class="form-control" type="text" name="usertype" placeholder="User Type">--}}
+
+                    <!-- Change to Radio Button -->
+                    <input class="form-control" type="submit" name="submit" value="Register" id="register-button">
+                    <small id = "register-reminder"class="form-text text-muted">*All fields are required</small>
+                    {{ csrf_field() }}
+                </form>
+            </div>
+            <span class="col-md-3"></span>
+        </div>
     </div>
-</div>
 <!-- Insert JS -->
 </body>
 </html>
